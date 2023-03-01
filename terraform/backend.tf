3a42a6ace01b69f6e7dce20e36daaa39a052b7d6
Kyle Hodgetts <kyle.hodgetts@digital.justice.gov.uk>
2023-03-01 10:50:43 +0000

    add comment explaining where bucket will be specified

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,7 @@ terraform {
     encrypt = true
     key     = "terraform.tfstate"
     region  = "eu-west-2"
+    # bucket is omitted here because it is specified as part of the backend config for the environment
+    # terraform init -backend-config=environments/$ENVIRONMENT/backend.hcl
   }
 }
