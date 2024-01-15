bcdd63cfe2516d9105899f797ee577aa84ba9c50
Seb Norris <sebastian@automationlogic.com>
2024-01-15 16:08:49 +0000

    use sh not bash for health check command

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ module "container" {
     }
   }
   healthcheck = {
-    command     = ["bash", "wget --spider --server-response http://localhost:5000/User/Login?ReturnURL=%2f 2>&1 | grep -q '200 OK' || exit 1"]
+    command     = ["CMD-SHELL", "wget --spider --server-response http://localhost:5000/User/Login?ReturnURL=%2f 2>&1 | grep -q '200 OK' || exit 1"]
     interval    = 30
     retries     = 3
     startPeriod = 60
