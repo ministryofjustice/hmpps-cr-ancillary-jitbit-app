c558c94952f114e56a9d56d1496520e7197740e4
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-04 11:15:54 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "ecs_cluster_arn" {
-  value = "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:cluster/hmpps-${var.environment}-${local.app_name}"
+  value = "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:cluster/hmpps-${var.environment}-${local.app_name}${var.suffix}"
 }
 
 output "ecs_service_arn" {
