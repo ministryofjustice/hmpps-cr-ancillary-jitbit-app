4b35ace1d174bf49a8222b7b3426d532fad240bb
David Teeling <david.teeling1@justice.gov.uk>
2025-10-30 16:13:23 +0000

    Correct image tag

--- terraform/sandbox_outputs.tf
+++ terraform/sandbox_outputs.tf
@@ -3,11 +3,11 @@ output "listener_arn" {
 }
 
 output "ecs_service_arn_blue" {
-  value = var.sub_env == "sandbox" && var.blue_image_tag != "" ? module.blue_deploy[0].service_arn : null
+  value = var.sub_env == "sandbox" && var.blue_image_tag != "" ? module.blue_deploy[0].service_arn : ""
 }
 
 output "ecs_service_arn_green" {
-  value = var.sub_env == "sandbox" && var.green_image_tag != "" ? module.green_deploy[0].service_arn : null
+  value = var.sub_env == "sandbox" && var.green_image_tag != "" ? module.green_deploy[0].service_arn : ""
 }
 
 output "target_group_arn_blue" {
